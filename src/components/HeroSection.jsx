f40637c1bb02e27653a988a32ff1b0f00fa7f343
sylvain.lafaye <sylvain-lafaye@hotmail.fr>
2024-10-23 13:31:44 +0200

    fix css

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,28 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function HeroSection() {
   return (
     <section className="h-screen flex flex-col items-center justify-center lg:flex-row lg:justify-normal lg:relative lg:overflow-x-hidden">
-        <article className="flex flex-col items-center text-center gap-4 px-2 md:gap-6 lg:items-start lg:text-start lg:ml-28">
-          <p className="text-xl text-wrap md:text-3xl lg:text-2xl">
-            Un collectif de jeunes développeurs qui a soif d&apos;experiences
-          </p>
-          <p className="text-xl text-wrap md:text-3xl lg:text-2xl">
-            Touchez un nouveau public avec
-            <span className="font-bold "> votre propre site internet !!</span>
-          </p>
-          <Link
-            to="/contact"
-            aria-label="contact"
-            className="contact-btn text-lg font-bold flex justify-center w-80 lg:inline"
-          >
-            <span className="marquee" aria-hidden>
-              Contactez-nous
-            </span>
-          </Link>
-        </article>
-        <img src="images\mockup.png" className="lg:absolute lg:left-2/3" alt="image d'un écran d'ordinateur" />
-      </section>
-  )
+      <article className="flex flex-col items-center text-center gap-4 px-2 md:gap-6 lg:items-start lg:text-start lg:ml-28">
+        <p className="text-xl text-wrap md:text-3xl lg:text-2xl">
+          Un collectif de jeunes développeurs qui a soif d&apos;experiences
+        </p>
+        <p className="text-xl text-wrap md:text-3xl lg:text-2xl">
+          Nous vous accompagnons dans la création de
+          <span className="font-bold "> votre premier site vitrine.</span>
+        </p>
+        <Link
+          to="/contact"
+          aria-label="contact"
+          className="contact-btn text-lg font-bold flex justify-center w-80 lg:inline"
+        >
+          <span className="marquee" aria-hidden>
+            Contactez-nous
+          </span>
+        </Link>
+      </article>
+      <img
+        src="images\mockup.png"
+        className="lg:absolute lg:left-2/3"
+        alt="image d'un écran d'ordinateur"
+      />
+    </section>
+  );
 }
